726ad7f5bf06714bfa8f7901598b2ab76ede30f3
Daboss <massamba.fall92@gmail.com>
2017-04-05 13:36:58 +0000

    configuration des utilisateur.

--- modele/users.php
+++ modele/users.php
@@ -6,4 +6,35 @@
  * Time: 10:52
  */
 
+function tag( $text , $color ){
+    return "<div class='w3-tag  $color ' style='padding:2px;margin:2px;'>
+            <div class='w3-tag $color w3-border w3-border-white'>" .
+            "<span class='w3-small'>".  $text ." </span>".
+            "</div></div>" ;
+}
+
+function privilege( $privNumber ){
+
+    $arr = array(
+       "read"   => tag(" Lire reporting " , "w3-blue"),
+       "create" => tag(" Créer reporting " , "w3-khaki" ) ,
+       "import" => tag(" Importer fichier " , "w3-green") ,
+       "admin" =>  tag(" Administrer " , "w3-red")
+    );
+
+    if( $privNumber == 0 )
+        return $arr['read'] ;
+
+    if( $privNumber == 1 )
+        return  $arr['create'] . $arr['read'] ;
+
+    else if( $privNumber == 2 )
+        return $arr['import'] . $arr['create'] . $arr['read']  ;
+
+    else if($privNumber == 3 )
+        return  $arr['admin'] . $arr['import'] . $arr['create'] . $arr['read']   ;
+
+    return;
+}
+
 include 'vue/app/utilisateur.php';
--- vue/app/utilisateur.php
+++ vue/app/utilisateur.php
@@ -5,3 +5,35 @@
  * Date: 05/04/2017
  * Time: 11:49
  */
+?>
+
+<h3 class="w3-padding-0 w3-text-grey"> <i class="fa fa-users"></i>
+    UTILISATEURS DU SERVICE <?= $_SESSION['service']['nom']; ?> <hr style='border-top:solid 1px #ccc;'>
+</h3>
+
+<div class="w3-container w3-border w3-white w3-padding-0 ">
+    <div class="w3-row">
+        <div class="w3-quarter w3-padding-large">
+            bonjour
+        </div>
+        <div class="w3-threequarter" style="border-left: 1px solid #ccc ;">
+            <table class="w3-table-all w3-hoverable ">
+                <thead> <th>Avatar</th> <th>Prénom</th>  <th>Nom</th> <th>Login</th> <th> Email </th> <th>privilèges</th></thead>
+                <tbody>
+                   <?php  foreach ( Database::getDb()->all('user' , "service" , $_SESSION['service']['id'] ) as $user ): ?>
+                    <tr style="cursor: pointer;">
+                        <td><img src="<?= URL."/image/avatar/".$user['icon']; ?>" class="w3-circle" style="height:50px;"></td>
+                        <td><?= $user['nom'];  ?></td>
+                        <td><?= $user['prenom'];  ?></td>
+                        <td><?= $user['login'];  ?></td>
+                        <td><?= $user['email'];  ?></td>
+                        <td><?= privilege($user['privileges']);  ?></td>
+                    </tr>
+                   <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        
+    </div>
+</div>
+
